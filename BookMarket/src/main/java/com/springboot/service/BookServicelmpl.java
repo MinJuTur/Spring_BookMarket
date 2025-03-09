2cf91a664a5703c4106bebc42cd7c1694427f3a5
JangMinJu <jmj4701jmj@gmail.com>
2025-03-09 11:44:18 +0900

    3장 내용

---
+++ BookMarket/src/main/java/com/springboot/service/BookServicelmpl.java
@@ -0,0 +1,23 @@
+package com.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.domain.Book;
+import com.springboot.repository.BookRepository;
+
+@Service
+public class BookServicelmpl implements BookService {
+	
+	@Autowired
+	
+	private BookRepository bookRepository;
+	
+	@Override
+	public List<Book> getAllBookList() {
+		return bookRepository.getAllBookList();
+	}
+
+}
